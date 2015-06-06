c5ad9e1e3b10ddf78a06a3113e9b537d6f07746f
Yaliang Wang <yaliangwang2015@u.northwestern.edu>
2015-06-05 19:41:16 -0500

    add 2D feature image

--- lib/tsne_python/tsne.py
+++ lib/tsne_python/tsne.py
@@ -161,12 +161,14 @@ def tsne(X = Math.array([]), no_dims = 2, initial_dims = 50, perplexity = 30.0):
 			
 	# Return solution
 	return Y;
+
+Y = 0
 		
 	
 if __name__ == "__main__":
 	print "Run Y = tsne.tsne(X, no_dims, perplexity) to perform t-SNE on your dataset."
 	print "Running example on 2,500 MNIST digits..."
-	X = Math.loadtxt("mnist2500_X.txt");
-	labels = Math.loadtxt("mnist2500_labels.txt");
-	Y = tsne(X, 2, 50, 20.0);
+	X = Math.loadtxt("FeaturedUsersTag.txt");
+	labels = Math.loadtxt("UserLabel.txt");
+	Y = tsne(X, 2, 12649, 20.0);
 	Plot.scatter(Y[:,0], Y[:,1], 20, labels);
